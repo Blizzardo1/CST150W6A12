47591b8c47f39be73745faaced2fabab51017a0c
Adonis Deliannis <Blizzardo1@Blizzeta.net>
2021-09-03 21:57:15 -0700

    Add project files.

---
+++ CST150W6A12/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CST150W6A12
+{
+    internal static class Extensions
+    {
+        private static readonly string _nonAlphanumeric = "[^a-zA-Z]";
+        public static string TrimSpecialCharacters(this string s) => Regex.Replace(s, _nonAlphanumeric, "");
+    }
+}
---
+++ CST150W6A12/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+
+namespace CST150W6A12
+{
+    internal static class Program
+    {
+
+        private static string OpenFile(string filename) => File.ReadAllText(filename);
+
+        private static string GetStatement()
+        {
+            Console.WriteLine("Enter a sentence or two. Press enter to begin checking for words that end with a 't' or an 'e'.\n");
+            Console.Write("Statement(s)> ");
+            return Console.ReadLine();
+        }
+
+        private static void Main(string[] args)
+        {
+            string contents;
+            if (args.Length < 1)
+            {
+                contents = GetStatement();
+            }
+            else
+            {
+                if (!File.Exists(args[0]))
+                {
+                    Console.WriteLine($"The file \"{args[0]}\" does not exist!");
+                    return;
+                }
+                contents = OpenFile(args[0]);
+            }
+
+            int count = 0;
+            foreach (string s in contents.Split(' '))
+            {
+                if (!s.TrimSpecialCharacters().EndsWith('t') && !s.TrimSpecialCharacters().EndsWith('e'))
+                    continue;
+
+                count++;
+            }
+
+            Console.WriteLine($"There are {count} words that end with a 't' or an 'e'");
+        }
+    }
+}
